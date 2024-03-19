80220870cb61cb4c542b3a3082bd0ab2e797e6cc
Lucas Moya <lucasmoyafe@gmail.com>
2024-03-19 18:02:12 -0300

    solucionado el flex del nav

--- src/components/CartWidget.jsx
+++ src/components/CartWidget.jsx
@@ -3,11 +3,11 @@ import { faCartShopping } from "@fortawesome/free-solid-svg-icons";
 
 export const CartWidget = () => {
   return (
-    <button type="button" className="btn btn-primary position-relative">
-      <FontAwesomeIcon icon={faCartShopping} />
-      <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
-        1
-      </span>
-    </button>
+      <button type="button" className="btn btn-primary position-relative">
+        <FontAwesomeIcon icon={faCartShopping} />
+        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+          1
+        </span>
+      </button>
   );
 };
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,43 +10,35 @@ import { CartWidget } from "./CartWidget";
 export const NavBar = () => {
   return (
     <Navbar expand="lg" bg="dark" data-bs-theme="dark">
-      <Container fluid>
-        <div className="row">
-          <div className="col-lg-2">
-            <Navbar.Brand className="" href="#home">
-              <Brand />
-            </Navbar.Brand>
-          </div>
-          <div className="col-lg-10">
-            <Navbar.Toggle aria-co ntrols="basic-navbar-nav" />
-            <Navbar.Collapse id="basic-navbar-nav">
-              <Nav className="me-auto ">
-                <Nav.Link href="#home">Inicio</Nav.Link>
-                <Nav.Link href="#link">Nosotros</Nav.Link>
-                <Nav.Link href="#home">Productos</Nav.Link>
-                <Nav.Link href="#link">Contacto</Nav.Link>
-                <NavDropdown title="Soporte" id="basic-nav-dropdown">
-                  <NavDropdown.Item href="#action/2.1">
-                    Servicio Técnico
-                  </NavDropdown.Item>
-                  <NavDropdown.Item href="#action/2.2">
-                    Garantias
-                  </NavDropdown.Item>
-                </NavDropdown>
-                <Form className="d-flex">
-                  <Form.Control
-                    type="search"
-                    placeholder="Search"
-                    className="me-2"
-                    aria-label="Search"
-                  />
-                  <Button variant="outline-success">Search</Button>
-                </Form>
-                <CartWidget />
-              </Nav>
-            </Navbar.Collapse>
-          </div>
-        </div>
+      <Container>
+        <Navbar.Brand className="" href="#home">
+          <Brand />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Inicio</Nav.Link>
+            <Nav.Link href="#link">Nosotros</Nav.Link>
+            <Nav.Link href="#home">Productos</Nav.Link>
+            <Nav.Link href="#link">Contacto</Nav.Link>
+            <NavDropdown title="Soporte" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/2.1">
+                Servicio Técnico
+              </NavDropdown.Item>
+              <NavDropdown.Item href="#action/2.2">Garantias</NavDropdown.Item>
+            </NavDropdown>
+            <Form className="d-flex">
+              <Form.Control
+                type="search"
+                placeholder="Search"
+                className="me-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Nav>
+        </Navbar.Collapse>
+        <CartWidget />
       </Container>
     </Navbar>
   );
