5a055c0ae2b48a5032e401d7d25409b59509a2e7
Lucas Moya <lucasmoyafe@gmail.com>
2024-04-24 12:35:49 -0300

    agregado el localstorage, falla el componente cart al refrescar, dejan de funcionar los botones y el cartwidget no va sincronizado con la cantidad de productos

--- src/App.jsx
+++ src/App.jsx
@@ -5,78 +5,25 @@ import { NavBar } from "./components/NavBar";
 import { ItemListContainer } from "./components/ItemListContainer";
 import { ItemDetailContainer } from "./components/ItemDetailContainer";
 import { PageNotFound } from "./components/PageNotFound";
-import { CartContext } from "./context/CartContext";
 import { MyForm } from "./components/MyForm";
-import { useState } from "react";
 import { Cart } from "./components/Cart";
-import { agregarAlCarrito } from "./helpers/agregarAlCarrito";
+import { CartProvider } from "./context/CartContext"; // Importa CartProvider
 
 function App() {
-  
-  const [cantidad, setCantidad] = useState(1);
-
-  const [item, setItem] = useState(null);
-
-  const handleSumar = (cantidad, setCantidad, item) => {
-    if (item && item.stock > cantidad) {
-      setCantidad(cantidad + 1);
-    }
-  };
-
-  const handleRestar = (cantidad, setCantidad) => {
-    cantidad > 1 && setCantidad(cantidad - 1);
-  };
-
-  const handleAgregarAlCarrito = (item, cantidad) => {
-    agregarAlCarrito(item, cantidad, carrito, setCarrito);
-  };
-
-  const [carrito, setCarrito] = useState([]);
-
-  const cantidadEnCarrito = () => {
-    return carrito.reduce((acc, prod) => acc + prod.cantidad, 0);
-  };
-
-  const mostrarCarrito = cantidadEnCarrito() > 0;
-
-  const vaciarCarrito = () => {
-    if (setCarrito) {
-      setCarrito([]);
-    } else {
-      return null;
-    }
-  };
-
   return (
-    <CartContext.Provider
-      value={{
-        item,
-        setItem,
-        cantidad,
-        setCantidad,
-        carrito,
-        handleSumar,
-        handleRestar,
-        handleAgregarAlCarrito,
-        cantidadEnCarrito,
-        vaciarCarrito,
-      }}
-    >
+    <CartProvider>
       <BrowserRouter>
         <NavBar />
         <Routes>
           <Route path="/" element={<ItemListContainer />} />
           <Route path="/contacto" element={<MyForm />} />
-          <Route
-            path="/carrito"
-            element={<Cart mostrarCarrito={mostrarCarrito} />}
-          />
+          <Route path="/carrito" element={<Cart />} /> {/* Elimina la prop mostrarCarrito */}
           <Route path="/category/:id" element={<ItemListContainer />} />
           <Route path="/item/:id" element={<ItemDetailContainer />} />
           <Route path="*" element={<PageNotFound />} />
         </Routes>
       </BrowserRouter>
-    </CartContext.Provider>
+    </CartProvider>
   );
 }
 
--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -1,2 +1,63 @@
-import { createContext } from "react";
+import { createContext, useEffect, useState } from "react";
+import { agregarAlCarrito } from "../helpers/agregarAlCarrito";
+
 export const CartContext = createContext();
+
+export const CartProvider = ({ children }) => {
+  const [cantidad, setCantidad] = useState(1);
+  const [item, setItem] = useState(null);
+  const carritoLocalStorage = JSON.parse(localStorage.getItem("carrito")) || [];
+  const [carrito, setCarrito] = useState(carritoLocalStorage);
+
+  const handleSumar = (cantidad, setCantidad, item) => {
+    if (item && item.stock > cantidad) {
+      setCantidad(cantidad + 1);
+    }
+  };
+
+  const handleRestar = (cantidad, setCantidad) => {
+    cantidad > 1 && setCantidad(cantidad - 1);
+  };
+
+  const handleAgregarAlCarrito = (item, cantidad) => {
+    agregarAlCarrito(item, cantidad, carrito, setCarrito);
+  };
+
+  const cantidadEnCarrito = () => {
+    return carrito.reduce((acc, prod) => acc + prod.cantidad, 0);
+  };
+
+  useEffect(() => {
+    localStorage.setItem("carrito", JSON.stringify(carrito));
+  }, [carrito]);
+
+  const mostrarCarrito = cantidadEnCarrito() > 0;
+
+  const vaciarCarrito = () => {
+    if (setCarrito) {
+      setCarrito([]);
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        item,
+        setItem,
+        cantidad,
+        setCantidad,
+        carrito,
+        mostrarCarrito,
+        handleSumar,
+        handleRestar,
+        handleAgregarAlCarrito,
+        cantidadEnCarrito,
+        vaciarCarrito,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
